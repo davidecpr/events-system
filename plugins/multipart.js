4285053b971e19d6a73571ff286c833d97d711ec
Davide Capriglione <davidecpr98@gmail.com>
2021-01-25 15:05:58 +0100

    #11

--- plugins/multipart.js
+++ plugins/multipart.js
@@ -5,7 +5,10 @@ const fp = require('fastify-plugin')
 module.exports = fp(async (fastify, opts) => {
   // register fastify-multipart plugin
   const options = {
-    attachFieldsToBody: true
+    attachFieldsToBody: true,
+    limits: {
+      fileSize: 1000000
+    }
   }
 
   fastify.register(require('fastify-multipart'), options)
